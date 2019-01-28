7f25861871d095fe7a2c920e3cd9a2584a83f856
Your Name <dorian.l.wallace@gmail.com>
2019-01-28 11:08:35 -0500

    first commit

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+drop table if exists albums;
+create table albums (
+id Int unsigned not null auto_increment,
+artist_name varchar(50) not null,
+record_name varchar(100) not null,
+release_date date not null,
+sales float(8,2) not null,
+genre varchar (50) not null,
+primary key (id)
+)
