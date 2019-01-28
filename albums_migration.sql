f88a906cd03dbf5dbb06e3529e88849f695ccee8
Your Name <dorian.l.wallace@gmail.com>
2019-01-28 17:00:30 -0500

    working on basic statements exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ create table albums (
 id Int unsigned not null auto_increment,
 artist_name varchar(50) not null,
 record_name varchar(100) not null,
-release_date year not null,
+release_date Int(4) not null,
 sales float(8,2) not null,
 genre varchar (50) not null,
 primary key (id)
